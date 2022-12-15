e4bda72bb3cf04c3faead1bd69e39c09624173af
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2022-12-15 18:06:58 +0000

    added support for stackable token minting and transfers

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -22,10 +22,15 @@
 #![allow(clippy::unused_unit)]
 
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::{ensure, pallet_prelude::*, traits::{Get, Currency, ReservableCurrency}, BoundedVec, Parameter};
+use frame_support::{
+	ensure,
+	pallet_prelude::*,
+	traits::{Currency, Get, ReservableCurrency},
+	BoundedVec, Parameter,
+};
 use scale_info::TypeInfo;
 use sp_runtime::{
-	traits::{AtLeast32BitUnsigned, CheckedAdd, CheckedSub, MaybeSerializeDeserialize, Member, One, Zero},
+	traits::{AtLeast32BitUnsigned, CheckedAdd, CheckedSub, MaybeSerializeDeserialize, Member, One, Saturating, Zero},
 	ArithmeticError, DispatchError, DispatchResult, RuntimeDebug,
 };
 use sp_std::vec::Vec;
@@ -123,6 +128,12 @@ pub mod module {
 		CannotDestroyClass,
 		/// Failed because the Maximum amount of metadata was exceeded
 		MaxMetadataExceeded,
+		/// Invalid stackable NFT transfer (stored value is equal to zero)
+		InvalidStackableNftTransfer,
+		/// Invalid stackable NFT balance
+		InvalidStackableNftAmount,
+		/// The stackable collection already exists
+		StackableCollectionAlreadyExists,
 	}
 
 	/// Next available class ID.
@@ -337,7 +348,90 @@ impl<T: Config> Pallet<T> {
 		})
 	}
 
+	/// Checks if account owns NFT
 	pub fn is_owner(account: &T::AccountId, token: (T::ClassId, T::TokenId)) -> bool {
 		TokensByOwner::<T>::contains_key((account, token.0, token.1))
+	}
+
+	/// Mint stackable NFT
+	pub fn mint_stackable_nft(
+		owner: &T::AccountId,
+		class_id: T::ClassId,
+		metadata: Vec<u8>,
+		data: T::TokenData,
+		amount: BalanceOf<T>,
+	) -> Result<(T::TokenId, BalanceOf<T>), DispatchError> {
+		ensure!(amount > Zero::zero(), Error::<T>::InvalidStackableNftAmount);
+
+		NextTokenId::<T>::try_mutate(class_id, |id| -> Result<(T::TokenId, BalanceOf<T>), DispatchError> {
+			let bounded_metadata: BoundedVec<u8, T::MaxTokenMetadata> =
+				metadata.try_into().map_err(|_| Error::<T>::MaxMetadataExceeded)?;
+
+			let token_id = *id;
+			*id = id.checked_add(&One::one()).ok_or(Error::<T>::NoAvailableTokenId)?;
+
+			Classes::<T>::try_mutate(class_id, |class_info| -> DispatchResult {
+				let info = class_info.as_mut().ok_or(Error::<T>::ClassNotFound)?;
+				info.total_issuance = info
+					.total_issuance
+					.checked_add(&One::one())
+					.ok_or(ArithmeticError::Overflow)?;
+				Ok(())
+			})?;
+
+			let token_info = TokenInfo {
+				metadata: bounded_metadata,
+				owner: owner.clone(),
+				data,
+			};
+			Tokens::<T>::insert(class_id, token_id, token_info);
+			TokensByOwner::<T>::insert((owner, class_id, token_id), ());
+
+			// Not likely to happen but ensure that the stackable collection balance is not
+			// already set
+			ensure!(
+				Self::get_stackable_collections_balances((class_id, token_id, owner.clone())) == Zero::zero(),
+				Error::<T>::StackableCollectionAlreadyExists
+			);
+
+			StackableCollectionsBalances::<T>::insert((class_id, token_id, owner.clone()), amount);
+			StackableCollection::<T>::insert((class_id, token_id), ());
+			Ok((token_id, amount))
+		})
+	}
+
+	/// Transfer stackable NFT
+	pub fn transfer_stackable_nft(
+		from: &T::AccountId,
+		to: &T::AccountId,
+		asset_id: (T::ClassId, T::TokenId),
+		amount: BalanceOf<T>,
+	) -> DispatchResultWithPostInfo {
+		StackableCollectionsBalances::<T>::try_mutate(
+			(asset_id.0, asset_id.1, from.clone()),
+			|sender_balance| -> DispatchResultWithPostInfo {
+				StackableCollectionsBalances::<T>::try_mutate(
+					(asset_id.0, asset_id.1, to.clone()),
+					|receiver_balance| -> DispatchResultWithPostInfo {
+						ensure!(
+							amount > Zero::zero()
+								&& Self::get_stackable_collections_balances((asset_id.0, asset_id.1, from.clone()))
+									>= amount,
+							Error::<T>::InvalidStackableNftTransfer
+						);
+
+						*receiver_balance = receiver_balance.checked_add(&amount).ok_or(ArithmeticError::Overflow)?;
+						*sender_balance = sender_balance.checked_sub(&amount).ok_or(ArithmeticError::Overflow)?;
+
+						Ok(().into())
+					},
+				)
+			},
+		)
+	}
+
+	/// Checks if token is stackable
+	pub fn is_stackable(token: (T::ClassId, T::TokenId)) -> Result<bool, DispatchError> {
+		Ok(Self::get_stackable_collection(token).is_some())
 	}
 }
