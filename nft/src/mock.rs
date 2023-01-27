6f6c102db42c559223a5fdb4f4c75ef765c35fae
Justin <justin.phamnz@gmail.com>
2023-01-28 12:01:39 +1300

    Update mock and unit test

--- nft/src/mock.rs
+++ nft/src/mock.rs
@@ -3,53 +3,72 @@
 #![cfg(test)]
 
 use frame_support::{
-	construct_runtime,
-	traits::{ConstU32, ConstU64, Everything},
+    construct_runtime,
+    parameter_types,
+    traits::{ConstU32, ConstU64, Everything},
 };
 use sp_core::H256;
 use sp_runtime::{testing::Header, traits::IdentityLookup};
 
-use super::*;
-
 use crate as nft;
+
+use super::*;
 
 pub type AccountId = u128;
 pub type BlockNumber = u64;
+pub type Balance = u128;
 
 impl frame_system::Config for Runtime {
-	type Origin = Origin;
-	type Index = u64;
-	type BlockNumber = BlockNumber;
-	type Call = Call;
-	type Hash = H256;
-	type Hashing = ::sp_runtime::traits::BlakeTwo256;
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = ConstU64<250>;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type DbWeight = ();
-	type BaseCallFilter = Everything;
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-	type OnSetCode = ();
-	type MaxConsumers = ConstU32<16>;
+    type Origin = Origin;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Call = Call;
+    type Hash = H256;
+    type Hashing = ::sp_runtime::traits::BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = ConstU64<250>;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type DbWeight = ();
+    type BaseCallFilter = Everything;
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = ConstU32<16>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u64 = 1;
+}
+
+impl pallet_balances::Config for Runtime {
+    type Balance = Balance;
+    type DustRemoval = ();
+    type Event = Event;
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = frame_system::Pallet<Runtime>;
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = ();
+    type WeightInfo = ();
 }
 
 impl Config for Runtime {
-	type ClassId = u64;
-	type TokenId = u64;
-	type ClassData = ();
-	type TokenData = ();
-	type MaxClassMetadata = ConstU32<1>;
-	type MaxTokenMetadata = ConstU32<1>;
+    type ClassId = u64;
+    type TokenId = u64;
+    type ClassData = ();
+    type TokenData = ();
+    type MaxClassMetadata = ConstU32<1>;
+    type MaxTokenMetadata = ConstU32<1>;
+    type Currency = PalletBalances;
 }
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
@@ -62,6 +81,7 @@ construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+		PalletBalances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
 		NonFungibleTokenModule: nft::{Pallet, Storage, Config<T>},
 	}
 );
@@ -76,19 +96,19 @@ pub const TOKEN_ID_NOT_EXIST: <Runtime as Config>::TokenId = 100;
 pub struct ExtBuilder;
 
 impl Default for ExtBuilder {
-	fn default() -> Self {
-		ExtBuilder
-	}
+    fn default() -> Self {
+        ExtBuilder
+    }
 }
 
 impl ExtBuilder {
-	pub fn build(self) -> sp_io::TestExternalities {
-		let t = frame_system::GenesisConfig::default()
-			.build_storage::<Runtime>()
-			.unwrap();
+    pub fn build(self) -> sp_io::TestExternalities {
+        let t = frame_system::GenesisConfig::default()
+            .build_storage::<Runtime>()
+            .unwrap();
 
-		let mut ext = sp_io::TestExternalities::new(t);
-		ext.execute_with(|| System::set_block_number(1));
-		ext
-	}
+        let mut ext = sp_io::TestExternalities::new(t);
+        ext.execute_with(|| System::set_block_number(1));
+        ext
+    }
 }
