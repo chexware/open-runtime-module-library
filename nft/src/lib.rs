7dc8c2ae4256a791f760a2122f8cc77bad545811
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-12-02 18:39:52 +0000

    remove duplicated Pallet<T>

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -211,9 +211,6 @@ pub mod module {
             GenesisConfig { tokens: vec![] }
         }
     }
-	#[pallet::pallet]
-	#[pallet::without_storage_info]
-	pub struct Pallet<T>(_);
 
     #[pallet::genesis_build]
     impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
